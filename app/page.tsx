0cc19c53fd6ab58b3671aa386c0086a8227cba77
koskei-kipkoech <patrickwayy@gmail.com>
2025-04-25 11:28:31 +0300

    feat(home): add AboutPage component and update title
    Include the AboutPage component in the home layout and enhance the title with an emoji for better visual appeal

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 import { Navbar } from "@/components/ui/navbar";
 import BackgroundPaths from "@/components/home";
+import AboutPage from "./about/page";
 
 export default function Home() {
   return (
     <div className="relative min-h-screen">
       <Navbar />
-      <BackgroundPaths title="Health" />
+      <BackgroundPaths title="Health💉" />
+      <AboutPage />
     </div>
   );
 }
